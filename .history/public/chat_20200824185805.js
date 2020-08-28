175529b3894c8248d4aafb0c88b69a504bc8fb01
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-08-28 21:32:27 +0800

    chat box test

---
+++ .history/public/chat_20200824185805.js
@@ -0,0 +1,22 @@
+// Make connection
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+
+// Emit events (see the index.js to know how it handle)
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+  message.value = "";
+});
+
+// Listen for events (retrieve event from the server)
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
---
+++ .history/public/chat_20200824221817.js
@@ -0,0 +1,33 @@
+// Make connection
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+    feedback = document.getElementById('feedback');
+// Emit events (see the index.js to know how it handle)
+btn.addEventListener('click', function(){
+  socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+  });
+  message.value = "";
+});
+
+message.addEventListener('keypress',function() {
+    socket.emit('typing',handle.value);
+});
+
+// Listen for events (retrieve event from the server)
+socket.on('chat', function(data){
+    feedback.innerHTML = ""; //Clear the typing queue while have submitted the message
+    //Modify the message in HTMLs
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing',function(data) {
+    // the data here is the handle.value (since the action is 'typing')
+    feedback.innerHTML = '<p><em>'+data+'is typing message...'+'</em></p>';
+});
